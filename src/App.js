6329054f1e093d11c480c5b49b07e82ba3910924
RyanMoore145 <techdendweller@gmail.com>
2025-04-28 00:59:38 -0300

    Animated name and contacts

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+	return (
+	<div className="App">
+		<div className="Typewriter">
+			<h1>Ryan Moore</h1>
+		</div>
+		<div className="FadeIn">
+			<h3>ry331186@dal.ca</h3>
+			<h3>(902) 321-6542</h3>
+		</div>
+	</div>
+	);
+}
+
+export default App;
