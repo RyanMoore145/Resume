7b05a82bb45ae170da35716277fa425f329b516c
RyanMoore145 <techdendweller@gmail.com>
2025-04-28 17:05:38 -0300

    started work experience and skills bar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
 				<img src="R.svg"></img>
 				<a href="#aboutMe">About Me</a>
 				<a href="#education">Education</a>
+				<a href="#workExperience">Work Experience</a>
+				<a href="#skillsBar">Skills</a>
 			</div>
 		</nav>
 		<header>
@@ -24,27 +26,73 @@ function App() {
 		<main>
 			<div id="mainNarrow">
 				<section id="aboutMe">
-					<h4>About Me</h4>
+					<h3>About Me</h3>
 					<p>Dedicated university student looking for reliable work. In the Computer Science program at Dalhousie University. People-oriented, good listening skills with experience working in a team environment.</p>
 					<p>Graduated from high school with Honours with distinction, received the Paul Bethune Memorial Award for excellence in math, and received the West Kings Faculty Award.</p>
 				</section>
 				<section id="education">
-					<h4>Education</h4>
+					<h3>Education</h3>
+					<h4>Bachelor's of Computer Science</h4>
 					<div className="table">
 						<div className="EducationTableElement" id="dal">
 							<img src="dalhousie.jpeg"></img>
 							<h4>Dalhousie</h4>
+							<p> Sept 2022 - Present</p>
 						</div>
 						<div className="EducationTableElement" id="acadia">
 							<img src="acadia.jpg"></img>
 							<h4>Acadia</h4>
+							<p>Sept 2021 - May 2022</p>
 						</div>
 					</div>
+				</section>
+				<section id="workExperience">
+					<h3>Work Experience</h3>
+					<div className="WorkElement">
+						<h4>Telemarketing Representative</h4>
+						<p>East Coast Water Solutions - Dartmouth NS</p>
+						<p>Jun 2024 - Sept 2024</p>
+						<p>Contacted potential customers to promote water quality tests and schedule appointments.</p>
+						<p>Educated callers on the benefits of water testing and addressed inquiries.</p>
+						<p>Maintained an organized call log and appointment schedule.</p>
+						<p>Followed scripts and adapted messaging to engage customers affectively.</p>
+						<p>Provided excellent customer service and handled objections professionally.</p>
+					</div>
+					<div className="WorkElement">
+						<h4>Weapons Technician, Reservist</h4>
+						<p>Canadian Armed Forces - Halifax NS</p>
+						<p>Nov 2021 - Jan 2024</p>
+						<p>Weapons Technicians are responsible for maintaining functional small arms and servicing non-functional small arms belonging to their unit.</p>
+						<p>Completed the Basic Training course, strengthening abilities in team building, time and stress management, quick critical thinking, and patience in a field and classroom environment.</p>
+						<p>Served two summers at Willow Park Armouries completing secondary courses including first aid training, maintaining inventory in the storage bay, and fulfilling general duties.</p>
+					</div>
+					<div className="WorkElement">
+						<h4>AI Trainer</h4>
+						<p>DataAnnotation</p>
+						<p>Mar 2024 - Present</p>
+						<p>Evaluated and corrected AI responses to given prompts pertaining to subjects such as coding, life advice, and general knowledge from a work from home environment.</p>
+					</div>
+				</section>
+			</div>
+			<div id="skillsBar">
+				<h3>Skills</h3>
+				<section>
+					<h4>React / React Native</h4>
+					<p>Years of experience: 1</p>
+				</section>
+				<section>
+					<h4>C++</h4>
+					<p>Years of experience: 4</p>
+				</section>
+				<section>
+					<h4>Python</h4>
+					<p>Years of experience: 3</p>
 				</section>
 			</div>
 		</main>
 		<footer>
-		
+			<h4>Source code:</h4>
+			<a href="https://github.com/RyanMoore145/CV">https://github.com/RyanMoore145/CV</a>
 		</footer>
 	</div>
 	);
